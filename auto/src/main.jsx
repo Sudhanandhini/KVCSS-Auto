3f58a054fda37bb9a57fadb3fc4d0c2bf3d21f6a
Sudhanandhini <sudhanandhinis@gmail.com>
2025-10-02 10:44:25 +0530

    main.jsx

--- auto/src/main.jsx
+++ auto/src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'   // 👈 Import HashRouter
+// import { HashRouter } from 'react-router-dom'   // 👈 Import HashRouter
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 // Import Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -11,8 +12,8 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/KVCSS-Auto">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )

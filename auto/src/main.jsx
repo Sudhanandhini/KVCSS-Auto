79b21319532a2154ed524a6711fa78d75682cf86
Sudhanandhini <sudhanandhinis@gmail.com>
2025-10-02 10:49:22 +0530

    hash router add

--- auto/src/main.jsx
+++ auto/src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import { HashRouter } from 'react-router-dom'   // 👈 Import HashRouter
+import { HashRouter } from 'react-router-dom'   // 👈 Import HashRouter
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 
 // Import Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -12,8 +11,8 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/KVCSS-Auto">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
